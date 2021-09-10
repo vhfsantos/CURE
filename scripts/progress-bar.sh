7b65f8bd88454dbc85b3e6c0cbb54cb8a4ac74ff
Vinícius H F Santos <vinicius6.santos@usp.br>
2021-09-10 11:39:06 -0300

    Welcome message in log

--- scripts/progress-bar.sh
+++ scripts/progress-bar.sh
@@ -4,10 +4,10 @@
 # https://stackoverflow.com/questions/238073/how-to-add-a-progress-bar-to-a-shell-script
 
 let _progress=(${1}*100/${2}*100)/100
-let _done=(${_progress}*3)/10
-let _left=30-$_done
+let _done=(${_progress})/10
+let _left=10-$_done
 
 _fill=$(printf "%${_done}s")
 _empty=$(printf "%${_left}s")
 
-printf "\r└ progress: |${_fill// /▇}${_empty// /-}| ${_progress}%% ($1 of $2)"
+printf "|${_fill// /▇}${_empty// /-}| ${_progress}%% ($1 of $2)\r"
