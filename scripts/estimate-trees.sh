aa363e41527abf4cfbb880d36acc4219d2d50ceb
Vinícius Franceschini-Santos <vinicius6.santos@usp.br>
2022-08-05 20:15:14 -0300

    add UCERegion to estimate trees

--- scripts/estimate-trees.sh
+++ scripts/estimate-trees.sh
@@ -34,38 +34,56 @@ echo -e "
                    --iqtree-out <path/to/iqtree-out>
 
 \e[4mRequired arguments\e[0m:
- --cure-out                  Path to alignments produced by CURE
+ --gene-region-out           Path to alignments produced by CURE GeneRegion
+ 
+ --uce-region-out            Path to alignments produced by CURE UCERegion
+ 
  --estimated-trees           Output directory name
+
 
 \e[4mOptional arguments\e[0m:
  --threads                    Number of threads for the analysis (Default: 2)
+ 
  --only-by-gene               Only estimate trees for 'concatenated-by-gene/' files
+                              (valid only if --gene-region-out is provided)
+							  
  --only-by-genic-region       Only estimate trees for 'concatenated-by-genic-region/' files
+                              (valid only if --gene-region-out is provided)
+							  
  --custom-alignments          Path to a custom alignment dir. Estimate trees using
                               alignments in this dir, ignoring other inputs.
 
 
 \e[4mExamples\e[0m:
 
-To estimate trees from alignments produced by CURE, run:
+If you run CURE using both GeneRegion and UCERegion strategies, you can estimate
+the trees of both outputs with the following:
 
     estimate-trees.sh \\
-        --cure-out CURE-output/ \\
-	    --estimated-trees estimated-trees
+        --gene-region-out CURE-GeneRegion-output \\
+		--uce-region-out CURE-UCERegion-output \\
+        --estimated-trees estimated-trees
 
-To estimate trees only from alignments concatenated by gene, run:
+(If only one strategy was run, you can use the appropriate: --gene-region-out or --uce-region-out)
+
+---
+
+To estimate trees only from alignments concatenated by gene (output of CURE GeneRegion), run:
 
 	estimate-trees.sh \\
-	    --cure-out CURE-output/ \\
+	    --gene-region-out CURE-GeneRegion-output \\
 	    --estimated-trees estimated-trees \\
 	    --only-by-gene
+---
 
-To estimate trees only from alignments concatenated by genic region, run:
+To estimate trees only from alignments concatenated by genic region (output of CURE GeneRegion), run:
 
 	estimate-trees.sh \\
-	    --cure-out CURE-output/ \\
+	    --gene-region-out CURE-GeneRegion-output \\
 	    --estimated-trees estimated-trees \\
 	    --only-by-genic-region
+
+---
 
 Also, you can use this script to estimate gene trees in parallel from alignments
 produced by any other tool rather than CURE. For so, just use --custom-alignments
@@ -76,21 +94,21 @@ parameter:
 	    --estimated-trees estimated-trees
 
 
- ╔══════════════════════════════════════════════════════╗
- ║                          NOTE                        ║
- ╠══════════════════════════════════════════════════════╣
- ║  The parameter 'cure-out' must direct to the output  ║
- ║  of CURE. This script will enter this directory and  ║
- ║  look for the outputs produced by CURE.              ║
- ║  If you renamed them, it will raise an error.        ║
- ╚══════════════════════════════════════════════════════╝
+ ╔════════════════════════════════════════════════════════════════════════════════════╗
+ ║                                          NOTE                                      ║
+ ╠════════════════════════════════════════════════════════════════════════════════════╣
+ ║ The parameters '--gene-region-out' and '--uce-region-out' must direct to           ║
+ ║ the output directory produced by CURE GeneRegion and CURE UCERegion, respectively. ║ 
+ ║ This script will enter this directory and look for the outputs produced by CURE.   ║
+ ║ If you renamed any file or directory, it will raise an error.                      ║
+ ╚════════════════════════════════════════════════════════════════════════════════════╝
  "
 exit 2
 }
 
 # Option strings for arg parser
 SHORT=h
-LONG=help,cure-out:,estimated-trees:,threads:,only-by-gene,only-by-region,custom-alignments:
+LONG=help,gene-region-out:,uce-region-out:,estimated-trees:,threads:,only-by-gene,only-by-region,custom-alignments:
 
 # Set deafult values
 tmp=$(realpath "$0")
@@ -111,16 +129,18 @@ OPTS=$(getopt --options $SHORT --long $LONG --name "$0" -- "$@")
 if [ $? != 0 ]; then error_exit "Failed to parse options...exiting."; fi
 eval set -- "$OPTS"
 
-if [ $? != 0 ]; then error_exit "Failed to parse options...exiting."; fi
-
 # Extracting arguments
 while true ; do
 	case "$1" in
 		-h | --help )
 		usage
 		;;
-		--cure-out )
-		CURE_OUT="$2"
+		--gene-region-out )
+		GeneRegion_OUT="$2"
+		shift 2
+		;;
+		--uce-region-out )
+		UCERegion_OUT="$2"
 		shift 2
 		;;
 		--estimated-trees )
@@ -160,22 +180,32 @@ if [ -z "${OUT}" ]; then
 	error_exit "Please, supply the output directory name."
 fi
 
-if [ -z "${CURE_OUT}" ] && [ -z "${CUSTOM_ALI}" ]; then
- 	error_exit "Please, supply the input aligments directory."
+# Check if the outputs of CURE were provided
+if [ -z "${GeneRegion_OUT}" ] && [ -z "${UCERegion_OUT}" ]; then
+ 	error_exit "Please, supply any output of CURE with --gene-region-out or --uce-region-out"
+fi
+
+# Check if none input was provided
+if [ -z "${GeneRegion_OUT}" ] && [ -z "${UCERegion_OUT}" ] && [ -z "${CUSTOM_ALI}" ]; then
+ 	error_exit "Please, supply the input alignments directory."
 fi
 
 #creating outputs
 IQTREE_OUT="$OUT"/IQtree_output
 ASTRAL_IN="$OUT"/ASTRAL_input
 mkdir -p "$IQTREE_OUT" "$ASTRAL_IN"
-#---------------
-BY_GENE="$ASTRAL_IN"/by-gene
-BY_REGION="$ASTRAL_IN"/by-region
+#-------- GeneRegion
+GR_BY_GENE="$ASTRAL_IN"/GeneRegion_by-gene
+GR_BY_GENIC_REG="$ASTRAL_IN"/GeneRegion_by-genic-region
+#-------- UCERegion
+UR_ASTRAL="$ASTRAL_IN"/UCERegion
 
+# Func to estimate intergenic regions (GeneRegion)
+# Run as: input_dir output_dir task
 Run_IQtree_NEXUS() {
-	NEXUS_DIR="$CURE_OUT"/"$1"
-	OUT_DIR="$IQTREE_OUT"/"$1"
-	TODO="$2"
+	NEXUS_DIR="$1"
+	OUT_DIR="$2"
+	TODO="$3"
 	mkdir -p "${OUT_DIR}"
 	NEXUS=$(find "$NEXUS_DIR" -name *.nexus)
 	N_NEXUS=$(find "$NEXUS_DIR" -name *.nexus | wc -l)
@@ -197,10 +227,12 @@ Run_IQtree_NEXUS() {
 	echo "- Done"
 }
 
+# Func to estimate with other regions (GeneRegion) and partitioned UCEs (UCERegion)
+# Run as: input_dir output_dir task
 Run_IQtree_PHYLIP() {
-	PHYLIP_DIR="$CURE_OUT"/"$1"
-	OUT_DIR="$IQTREE_OUT"/"$1"
-	TODO="$2"
+	PHYLIP_DIR="$1"
+	OUT_DIR="$2"
+	TODO="$3"
 	mkdir -p "${OUT_DIR}"
 	PHYLIP=$(find "$PHYLIP_DIR" -name *.phylip)
 	N_PHYLIP=$(find "$PHYLIP_DIR" -name *.phylip | wc -l)
@@ -251,38 +283,69 @@ Run_IQtree_CUSTOM() {
 	echo "- Done"
 }
 
+
+
+# Run IQtree for custom data, if provided
 if [ -z "$CUSTOM_ALI" ]; then
+	echo "- No custom alignment were provided. Skipping..."
+else
+    echo "- Running IQ-Tree for custom alignment files"
+	echo "--------------------------------------------"
+	Run_IQtree_CUSTOM Run
+	echo "- Checking..."
+	Run_IQtree_CUSTOM Check
+	# ------------------------------------------------------
+	echo "- Preparing ASTRAL input for custom alignments"
+	cat "$IQTREE_OUT"/*treefile \
+		> "$ASTRAL_IN"/concatenated-trees.tre
+fi
+
+# Run IQtree for GeneRegion data, if provided
+
+if [ -z "$GeneRegion_OUT" ]; then
+	echo "- No alignments of CURE GeneRegion were provided. Skipping..."
+else
+    echo "- Running IQ-Tree for CURE GeneRegion files"
+	echo "-------------------------------------------"
 	# Running for intergenic regions
 	if [ -z "$(ls -A "$IQTREE_OUT"/"intergenic-regions")" ]; then
-		Run_IQtree_NEXUS intergenic-regions Run
+		Run_IQtree_NEXUS "$GeneRegion_OUT"/intergenic-regions \
+		                 "$IQTREE_OUT"/intergenic-regions \
+						 Run
 		echo "- Checking..."
-		Run_IQtree_NEXUS intergenic-regions Check
+		Run_IQtree_NEXUS "$GeneRegion_OUT"/intergenic-regions \
+		                 "$IQTREE_OUT"/intergenic-regions \
+						 Check
 	else
 		echo "- Already estimated trees of intergenic regions. Skipping..."
 	fi
 
-	# Running for concatenated by region
+	# Running for concatenated by genic region
 	if [ "$ONLY_BY_GENE" == "False" ]; then
 		if [ -z "$(ls -A "$IQTREE_OUT"/"concatenated-by-genic-region")" ]; then
-			Run_IQtree_NEXUS concatenated-by-genic-region Run
+			Run_IQtree_NEXUS "$GeneRegion_OUT"/concatenated-by-genic-region \
+			                 "$IQTREE_OUT"/concatenated-by-genic-region \
+							 Run
 			echo "- Checking..."
-			Run_IQtree_NEXUS concatenated-by-genic-region Check
+			Run_IQtree_NEXUS "$GeneRegion_OUT"/concatenated-by-genic-region \
+			                 "$IQTREE_OUT"/concatenated-by-genic-region \
+							 Check
 			# ------------------------------------------------------
-			echo "- Preparing ASTRAL input by genic region"
-			mkdir -p $BY_REGION
+			echo "- Preparing ASTRAL input (by genic region)"
+			mkdir -p $GR_BY_GENIC_REG
 			# only exons
 			cat "$IQTREE_OUT"/concatenated-by-genic-region/*__exon-*treefile \
-				> "$BY_REGION"/only-exons.tre
+				> "$GR_BY_GENIC_REG"/only-exons.tre
 			# only introns
 			cat "$IQTREE_OUT"/concatenated-by-genic-region/*__introns.treefile \
-				> "$BY_REGION"/only-introns.tre
+				> "$GR_BY_GENIC_REG"/only-introns.tre
 			# only genic regions (exons + introns)
-			cat "$BY_REGION"/only-introns.tre "$BY_REGION"/only-exons.tre \
-				> "$BY_REGION"/only-genic-regions.tre
+			cat "$GR_BY_GENIC_REG"/only-introns.tre "$GR_BY_GENIC_REG"/only-exons.tre \
+				> "$GR_BY_GENIC_REG"/only-exons-and-introns.tre
 			# all regions (exons + introns + intergenic regions)
 			cat "$IQTREE_OUT"/intergenic-regions/*treefile \
-				"$BY_REGION"/only-introns.tre "$BY_REGION"/only-exons.tre \
-				> "$BY_REGION"/all-regions.tre
+				"$GR_BY_GENIC_REG"/only-introns.tre "$GR_BY_GENIC_REG"/only-exons.tre \
+				> "$GR_BY_GENIC_REG"/all-regions.tre
 
 		else
 			echo "- Already estimated trees by genic region. Skipping..."
@@ -292,31 +355,55 @@ if [ -z "$CUSTOM_ALI" ]; then
 	# Create output concatenated by gene
 	if [ "$ONLY_BY_REGION" == "False" ]; then
 		if [ -z "$(ls -A "$IQTREE_OUT"/"concatenated-by-gene")" ]; then
-			Run_IQtree_PHYLIP concatenated-by-gene Run
+		    # $IQTREE_OUT"/"
+			Run_IQtree_PHYLIP "$GeneRegion_OUT"/concatenated-by-gene \
+			                  "$IQTREE_OUT"/concatenated-by-gene \
+							  Run
 			echo "- Checking..."
-			Run_IQtree_PHYLIP concatenated-by-gene Check
+			Run_IQtree_PHYLIP "$GeneRegion_OUT"/concatenated-by-gene \
+			                  "$IQTREE_OUT"/concatenated-by-gene \
+							  Check
 			# ------------------------------------------------------
-			echo "- Preparing ASTRAL input by gene"
-			mkdir -p $BY_GENE
+			echo "- Preparing ASTRAL input (by gene)"
+			mkdir -p $GR_BY_GENE
 			# only genic regions
 			cat "$IQTREE_OUT"/concatenated-by-gene/*treefile \
-				> "$BY_GENE"/only-genic-regions.tre
+				> "$GR_BY_GENE"/only-exons-and-introns.tre
 			# all regions (add intergenic)
 			cat "$IQTREE_OUT"/intergenic-regions/*treefile \
-				"$BY_GENE"/only-genic-regions.tre \
-				> "$BY_GENE"/all-regions.tre
+				"$GR_BY_GENE"/only-exons-and-introns.tre \
+				> "$GR_BY_GENE"/all-regions.tre
 		else
 			echo "- Already estimated trees by gene. Skipping..."
 		fi
 	fi
-else
-	Run_IQtree_CUSTOM Run
-	echo "- Checking..."
-	Run_IQtree_CUSTOM Check
-	# ------------------------------------------------------
-	echo "- Preparing ASTRAL input"
-	cat "$IQTREE_OUT"/*treefile \
-		> "$ASTRAL_IN"/concatenated-trees.tre
 fi
 
-echo "- All done. Bye"
+# Run IQtree for UCERegion data, if provided
+
+if [ -z "$UCERegion_OUT" ]; then
+	echo "- No alignments of CURE UCERegion were provided. Skipping..."
+else
+    echo "- Running IQ-Tree for CURE UCERegion files"
+	echo "------------------------------------------"
+	
+	Run_IQtree_PHYLIP "$UCERegion_OUT"/partitioned-uces \
+		             "$IQTREE_OUT"/UCERegion \
+					 Run
+	echo "- Checking..."
+	Run_IQtree_PHYLIP "$UCERegion_OUT"/partitioned-uces \
+		             "$IQTREE_OUT"/UCERegion \
+					 Check
+	# ------------------------------------------------------
+	mkdir -p "$UR_ASTRAL"
+	echo "- Preparing ASTRAL input for UCERegion files"
+	cat "$IQTREE_OUT"/UCERegion/*treefile \
+		> "$UR_ASTRAL"/partitioned-uces.tre
+
+echo "- All done!
+
+If you make use of CURE in your research, please cite:
+
+Freitas et al (2022). UCE phylogenomics, biogeography, and classification of long-horned 
+bees (Hymenoptera: Apidae: Eucerini), with insights on using specimens with extremely 
+degraded DNA. In prep."
